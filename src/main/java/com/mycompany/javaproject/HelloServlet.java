a9d03ca2aad73cad3ca4fff23ae8cf00783e9e81
Sam <hibbards01@gmail.com>
2015-06-12 17:29:45 -0600

    Change for openShift!

--- src/main/java/com/mycompany/javaproject/HelloServlet.java
+++ src/main/java/com/mycompany/javaproject/HelloServlet.java
@@ -40,7 +40,8 @@ public class HelloServlet extends HttpServlet {
             out.println("<title>Servlet HelloServlet</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet HelloServlet at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet HelloServlet at Openshift!</h1>");
+            out.println("<p>I am so glad this is working!</p>");
             out.println("</body>");
             out.println("</html>");
         }
