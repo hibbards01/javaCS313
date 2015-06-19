cb460bbf8aac1b9bdd2da5eee025ec91e61c247e
Sam <hibbards01@gmail.com>
2015-06-19 14:19:52 -0600

    Debugging Openshift 2

--- src/main/java/backend/Database.java
+++ src/main/java/backend/Database.java
@@ -59,8 +59,7 @@ public class Database {
         MYSQL_DRIVER = "com.mysql.jdbc.Driver";
         DB_URL = "jdbc:mysql://" + dbHost + ":" + dbPort + "/java_projects";
         USER = System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
-        //PASS = System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
-        PASS = "password";
+        PASS = System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
     }
     
     /**
